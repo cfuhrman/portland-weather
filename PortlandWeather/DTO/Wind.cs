d8bd8a5c55a09ea57b3ccfa6b2d2ea7cb87c3364
Christopher M. Fuhrman <cfuhrman@panix.com>
2024-12-13 08:05:28 -0800

    Convert floats to doubles

--- PortlandWeather/DTO/Wind.cs
+++ PortlandWeather/DTO/Wind.cs
@@ -5,11 +5,11 @@ namespace PortlandWeather.DTO;
 public class Wind
 {
     [JsonPropertyName("speed")]
-    public required float Speed {  get; set; }
+    public required double Speed {  get; set; }
 
     [JsonPropertyName("deg")]
     public required int Degree { get; set; }
 
     [JsonPropertyName("gust")]
-    public float? Gust { get; set; }
+    public double? Gust { get; set; }
 }
